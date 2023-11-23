f2ea05bf8d328df39ccc0c5165f4e4c977f88fd4
Anastasia030 <a.nizovtseva@g.nsu.ru>
2023-11-23 20:57:20 +0700

    update snowflake

--- snowflake.py
+++ snowflake.py
@@ -1,29 +1,29 @@
-from turtle import *
+import turtle
 
 
 def koch(order, size):
     if order == 0:
-        forward(size)
+        turtle.forward(size)
     else:
         koch(order-1, size/3)
-        left(60)
+        turtle.left(60)
         koch(order-1, size/3)
-        right(120)
+        turtle.right(120)
         koch(order-1, size/3)
-        left(60)
+        turtle.left(60)
         koch(order-1, size/3)
 
 
 def main():
-    up()
-    goto(-100, 0)
-    down()
+    turtle.up()
+    turtle.goto(-100, 0)
+    turtle.down()
     n = int(input('Глубина рекурсии:'))
     a = int(input('Длина стороны:'))
     for i in range(3):
         koch(n, a)
-        right(120)
+        turtle.right(120)
 
 
 main()
-mainloop()
+turtle.mainloop()
