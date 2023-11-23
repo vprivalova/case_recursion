6f241e5aef907c30e5d4beddba22d61816636146
Anastasia030 <a.nizovtseva@g.nsu.ru>
2023-11-23 21:37:02 +0700

    ice fractal 1

---
+++ ice_fractal_2.py
@@ -0,0 +1,34 @@
+import turtle
+
+
+def ice_fractal_2(n, size):
+    if n == 0:
+        turtle.forward(size)
+    else:
+        ice_fractal_2(n-1, size)
+        turtle.left(120)
+        ice_fractal_2(n-1, size / 2)
+        turtle.left(180)
+        ice_fractal_2(n - 1, size / 2)
+        turtle.left(120)
+        ice_fractal_2(n - 1, size / 2)
+        turtle.left(180)
+        ice_fractal_2(n - 1, size / 2)
+        turtle.left(120)
+        ice_fractal_2(n - 1, size)
+
+
+def main():
+    turtle.tracer(0)
+    turtle.up()
+    turtle.goto(-100, 0)
+    turtle.down()
+    n = int(input('Глубина рекурсии:'))
+    a = int(input('Длина стороны:'))
+    ice_fractal_2(n, a)
+    turtle.update()
+    turtle.done()
+
+
+main()
+turtle.mainloop()
