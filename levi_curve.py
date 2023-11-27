9401367e5f9fecf289212c8e7c4ea0759d7a3846
vprivalova <privalovaa.vv@gmail.com>
2023-11-27 18:42:09 +0700

    commit4

--- levi_curve.py
+++ levi_curve.py
@@ -1,4 +1,5 @@
 import turtle
+import ru_local as ru
 
 
 def levi(order, size):
@@ -19,6 +20,21 @@ def main_levicurve():
     turtle.down()
     n = int(input('Глубина рекурсии:'))
     a = int(input('Длина стороны:'))
+    available_colors = [ru.RED, ru.YELLOW, ru.GREEN, ru.BLUE, ru.BLACK, ru.VIOLET, ru.ORANGE]
+    available_colors_eng = {ru.RED: 'red', ru.YELLOW: 'yellow', ru.GREEN: 'green', ru.BLUE: 'blue',
+                            ru.BLACK: 'black', ru.VIOLET: 'violet', ru.ORANGE: 'orange'}
+
+    print(ru.ACCEPTABLE_COLORS)
+
+    for element in available_colors:
+        print(element)
+
+    color_name1 = input('\n' + ru.COLOR1_INPUT)
+    while color_name1 not in available_colors:
+        color_name1 = input(ru.COLOR1_INPUT_AGAIN)
+
+    color_name1 = available_colors_eng.setdefault(color_name1)
+    turtle.pencolor(color_name1)
     levi(n, a)
 
 
